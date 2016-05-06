fcd7fe8485053966e7a5be30938d0a75ceb4329e
Abhishek Gahlot <abhishekgahlot007@gmail.com>
2016-05-06 16:04:11 +0530

    Update checkdomain.py

--- checkdomain.py
+++ checkdomain.py
@@ -7,16 +7,16 @@ import urllib2
 from tabulate import tabulate
 
 def _check_domain(urls=['google.com']):
-	data = []
-	for url in urls:
-		try:
-			content = json.loads( urllib2.urlopen('https://domainr.com/api/json/info?&q='+url).read() )
-			data.append( [content['domain'],content['tld']['domain'],content['availability']] )
-		except urllib2.URLError as err:
-			print 'Internet Connection Error'
-			break
-	print tabulate(data,headers=['Domain Name','TLD','Availability'])
+  data = []
+  for url in urls:
+    try:
+      content = json.loads( urllib2.urlopen('https://domainr.com/api/json/info?&q='+url).read() )
+      data.append( [content['domain'],content['tld']['domain'],content['availability']] )
+    except urllib2.URLError as err:
+      print 'Internet Connection Error'
+      break
+    print tabulate(data,headers=['Domain Name','TLD','Availability'])
 
 
 if __name__ == "__main__":
-    _check_domain(sys.argv[1:])
+  _check_domain(sys.argv[1:])
